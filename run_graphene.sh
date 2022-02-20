8b7e5e2b167efe50095109aafb55900ba05d185f
StanPlatinum <weijliu@iu.edu>
2022-02-20 23:02:29 +0800

    update scripts and docker image names

--- run_graphene.sh
+++ run_graphene.sh
@@ -1,4 +1,4 @@
 #/bin/bash!
 
 sudo docker rm -f graphene-sgx
-sudo docker run -it --privileged -v /dev/sgx:/dev/sgx --name "graphene-sgx" graphene-sgx:v1.1
+sudo docker run -it --privileged -v /dev/sgx:/dev/sgx --name "graphene-sgx" liuweijie/graphene:v1.1
--- run_occlum.sh
+++ run_occlum.sh
@@ -1,2 +1,2 @@
 sudo docker rm -f occlum-sgx
-sudo docker run --cap-add sys_ptrace -it --name occlum-sgx --device /dev/sgx/enclave --device /dev/sgx/provision occlum-sgx:v0.26
+sudo docker run --cap-add sys_ptrace -it --name occlum-sgx --device /dev/sgx/enclave --device /dev/sgx/provision liuweijie/occlum:v0.26
